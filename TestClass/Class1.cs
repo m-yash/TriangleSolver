5b4c3cdaf6224843049d5ac10a95e4b45795c6e7
Yash <iamyashmistry@gmail.com>
2024-03-06 16:59:22 -0500

    Add project files.

---
+++ TestClass/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TriangleSolver;//
+
+
+namespace TriangleTest
+{
+    [TestFixture]
+    public class TriangleTest1
+    {
+        [Test]
+        public void ValidEquilateral_Input60and60and60_OutputValidEquilateral()
+        {
+            int firstSide = 1;
+            int secondSide = 1;
+            int thirdSide = 1;
+
+            string expected = "An equilateral triangle is formed";
+
+            string result = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
